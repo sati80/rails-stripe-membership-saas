5e91d387fb3664f6a66aa0f403cdc45d11f8ff32
sati80 <draford@gmail.com>
2012-11-28 20:12:31 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsStripeMembershipSaas::Application.config.secret_token = 'ab2ae4dfe91f81fa1407687c2c0e68b92bbbc00f65bfa6ddd01e37953a36e596708172760c7226805cb690bef24b92e83dfc728f31ff437bc4e3083494bcbdc9'
